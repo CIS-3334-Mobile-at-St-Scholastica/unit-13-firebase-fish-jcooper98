cbc9a117330c5143bb37d55633095561846e6ee8
jackcooper <hibjack@icloud.com>
2019-04-16 13:26:33 -0500

    Complete Participation

--- app/src/main/java/edu/css/unit13_firebasefish_2018/MainActivity.java
+++ app/src/main/java/edu/css/unit13_firebasefish_2018/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.css.unit13_firebasefish_2018;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,13 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -19,23 +27,26 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<Fish> fishAdapter;
     List<Fish> fishList;
     FishFirebaseData fishDataSource;
-//    DatabaseReference myFishDbRef;
+    DatabaseReference myFishDbRef;
     int positionSelected;
     Fish fishSelected;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //setupFirebaseDataChange();
+        setupFirebaseDataChange();
         setupListView();
         setupAddButton();
         setupDetailButton();
         setupDeleteButton();
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    /*
     private void setupFirebaseDataChange() {
         fishDataSource = new FishFirebaseData();
         myFishDbRef = fishDataSource.open();
@@ -55,7 +66,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    */
+
+
+
+
+
+
 
     private void setupListView() {
         listViewFish = (ListView) findViewById(R.id.ListViewFish);
